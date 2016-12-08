15367178a4f440a8a8292a0e157af66f14fab9df
vitalik <vitalik.parshin@gmail.com>
2016-12-08 23:57:29 +0200

    clear_code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './style.css';
-import InputText from "./apps/Inputname/Inputname";
-import ContactList from "./apps/Contactlist/ContactListES6";
-import Counter from "./apps/Counter/Counter";
-import Calcult from "./apps/Simple_Calculator/Own_calculator";
 import DifCulc from "./apps/Calculator/DifficultCalculator";
 
 
@@ -17,17 +13,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <hr />
-        <ContactList />
-        <hr/>
-        <InputText />
-        <Counter />
-        <hr/>
-        <Calcult />
-        <hr />
         <DifCulc />
       </div>
     );
